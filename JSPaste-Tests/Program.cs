37097e2d0058e6988aa9e6d10b812d682f7e666f
Mrgaton <arnaupost4@gmail.com>
2024-01-06 23:15:08 +0100

    Agregar archivos de proyecto.

---
+++ JSPaste-Tests/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSPaste_Tests
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            JSPaste.Class1
+        }
+    }
+}
---
+++ JSPaste/JSPaste.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.Common;
+using System.Net.Http;
+using System.Text;
+
+namespace JSPaste
+{
+    public class JSPaste
+    {
+        private readonly const string ServerEndPoint = "https://jspaste.eu/";
+
+        public static HttpClient httpClient = new HttpClient();
+        public static void Send(string data) => Send(data,Encoding.UTF8);
+        public static void Send(string data, Encoding enc) => Send(enc.GetBytes(data));
+        public static void Send(byte[] data)
+        {
+
+        }
+    }
+
+    public class JSDocument
+    {
+        private string _key { get; set; }
+        public string Key { get { return _key;  } }
+
+        private string _secret { get; set; }
+        public string Secret { get { return _secret; } }
+        public JSDocument(string key, string secret) 
+        {
+            _key = key;
+            _secret = secret;
+        }
+    }
+}
