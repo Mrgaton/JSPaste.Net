b06ad0c099a9a6b3375d470695e47b4fc9c19777
Mrgaton <arnaupost4@gmail.com>
2024-01-07 16:30:04 +0100

    Esperando backend

--- JSPaste/JSPaste.cs
+++ JSPaste/JSPaste.cs
@@ -1,31 +1,65 @@
 ﻿using System;
-using System.Data.Common;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 
-namespace JSPaste
+namespace JSPaste.Net
 {
-    public class JSPaste
+    public class JSPasteClient
     {
-        private readonly const string ServerEndPoint = "https://jspaste.eu/";
+        private const string ServerEndPoint = "https://jspaste.eu/";
 
-        public static HttpClient httpClient = new HttpClient();
-        public static void Send(string data) => Send(data,Encoding.UTF8);
-        public static void Send(string data, Encoding enc) => Send(enc.GetBytes(data));
-        public static void Send(byte[] data)
+        public static HttpClient httpClient = new HttpClient()
         {
+            BaseAddress = new Uri(ServerEndPoint)
+        };
 
+        public static void Send(string data, DocumentSettings settings = default) => Send(data, Encoding.UTF8, settings);
+
+        public static void Send(string data, Encoding enc, DocumentSettings settings = default) => Send(enc.GetBytes(data), settings);
+
+        public static void Send(byte[] data, DocumentSettings settings = default)
+        {
+            using (var req = new HttpRequestMessage(HttpMethod.Get, "/documents"))
+            {
+                req.Version = HttpVersion.Version20;
+            }
         }
+
+        public static string Get(string key) => Get(key, Encoding.UTF8);
+
+        public static string Get(string key, Encoding enc) => enc.GetString(GetRaw(key));
+
+        public static byte[] GetRaw(string key)
+        {
+            return null;
+        }
+
+        public static bool Remove(string key, string secret)
+        {
+            return false;
+        }
+    }
+
+    public class DocumentSettings
+    {
+        public static TimeSpan LiveSpan { get; set; } = TimeSpan.Zero;
+        public static string SecretMask { get; set; }
+        public DocumentSettings()
+        { }
     }
 
     public class JSDocument
     {
         private string _key { get; set; }
-        public string Key { get { return _key;  } }
+        public string Key
+        { get { return _key; } }
 
         private string _secret { get; set; }
-        public string Secret { get { return _secret; } }
-        public JSDocument(string key, string secret) 
+        public string Secret
+        { get { return _secret; } }
+
+        public JSDocument(string key, string secret)
         {
             _key = key;
             _secret = secret;
